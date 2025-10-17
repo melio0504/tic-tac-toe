6b7ebafd724bbb9b4f038ae7a02a389319789e10
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-10-18 00:10:00 +0800

    Add the comment that contains the initial plan for logic
    I want to learn proper programming. My imposter syndrome is freaking
    me out. I feel like useless.

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+// Create factory function with single instance for gameboard, displayController
+let Gameboard = function() {
+  // Create an array with 3x3
+
+  // Has player 1 and player 2 (Human or Computer)
+
+  // Evaluate 3 straight rows / columns / diagonal X or O counts as win
+
+  // Game is best of 5
+}
+
+// The focus is to make the Tic Tac Toe working in the console first.
+
+// The UI can be implemented later since we are first learning to implement the main logic
